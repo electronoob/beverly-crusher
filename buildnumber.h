91e2a78ec70c4ea32c046d1893bfb639ed405d67
electronoob <github@electronoob.com>
2014-10-04 00:38:04 +0100

    added arduino example of output.

--- buildnumber.h
+++ buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 102
-#define BUILDNUMBER_STR "102" 
+#define BUILDNUMBER 103
+#define BUILDNUMBER_STR "103" 
 
 #endif /* BUILD_NUMBER_H_ */
